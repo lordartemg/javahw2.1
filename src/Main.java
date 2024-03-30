6214fa521a92805d56bd1f74da68da7be0d47e4c
Grdilyan Artem <grd.artems@gmail.com>
2024-03-30 18:45:27 +0300

    Initital commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int price = 15_000;
+        int miles = (price / 20);
+        System.out.println("Ваши мили за перелет: " + miles);
+    }
+}
